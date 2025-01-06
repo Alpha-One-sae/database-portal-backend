a84087fedf6a41d9e9663c5bf69124938c17ca07
Chanpreet <ckaur2174@gmail.com>
2025-01-06 22:03:02 +0530

    changed by Chanpreet

--- app/api.py
+++ app/api.py
@@ -15,6 +15,6 @@ class test(BaseModel):
 async def home(requests:test):
     if (requests.data=="hello"):
         if(requests.timestamp=="10"):
-            return {"status":200 , "message":"data saved successfuly"}
+            return {"status":200 , "message":"data saved successfully"} 
         return {"status":203 , "message":"error in storing data(wrong timeline)"}
     return {"status":203 , "message":"error in storing data(wrong data)"}
